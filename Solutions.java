0579d0339f4732034976cc4a108965848bdef5f7
raprem143 <144882563+raprem143@users.noreply.github.com>
2023-09-29 01:10:23 +0530

    Add files via upload
    check for java code correct or wrong
    
    
    Signed-off-by: raprem143 <144882563+raprem143@users.noreply.github.com>

---
+++ Solutions.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+import java.util.*;
+public class Solutions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x>0){
+            System.out.println("x is greater than 0");
+        } else {
+            System.out.println("x is less than or equal 0");
+        }
+    }
+}
